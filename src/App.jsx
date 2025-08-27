19c046fe9fe356c1b42ebd1a0e2981be32eedbb4
Maximilian <scientificresearch309@gmail.com>
2025-08-27 23:11:39 +0200

    the tech books are listed there

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,20 @@ function App() {
   return (
     <div className="App">
       <h1>There are {techBooks.length} Tech Books there!</h1>
-      <p>test</p>
+      {/* {techBooks.map((b) => console.log(b))} */}
+      {techBooks.map((tb) => (
+        <div className="techbooks">
+          <div className="techbook">
+            <p className="title">
+              <span>Title:</span> {tb.title}
+            </p>
+            <p className="description">
+              <span>Description:</span> {tb.description}
+            </p>
+          </div>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 }
