9edf69a45a7a42913e116d8cf6482f8f0d15ff88
Maximilian <scientificresearch309@gmail.com>
2025-08-27 22:52:41 +0200

    displaying the number of books therer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import techBooks from './data/techbook.json';
+
+console.log(techBooks);
 
 function App() {
   return (
     <div className="App">
-      <h1>Tech Books</h1>
+      <h1>There are {techBooks.length} Tech Books there!</h1>
       <p>test</p>
     </div>
   );
